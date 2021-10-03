17eed21aa4408a49b116741d573be443b9fc66f1
Derek Garnett <drkgrntt@gmail.com>
2021-10-03 12:55:15 -0500

    string replaces

--- server/src/types.ts
+++ server/src/types.ts
@@ -14,10 +14,25 @@ export class SurveyResult {
 
   constructor(dbRow: any) {
     this.age = dbRow.demo_age
-    this.reside = dbRow.demo_reside
+    this.reside = SurveyResult.formatResidence(dbRow.demo_reside)
     this.showsPerYear = dbRow.demo_shows_per_year
     this.covidConcern = parseInt(dbRow.demo_covid_concern)
     this.shopTkts = dbRow.demo_shop_tkts
     this.date = dbRow.demo_date
+  }
+
+  static formatResidence(residence: string) {
+    switch (residence) {
+      case 'NYC':
+        return 'NYC Resident'
+      case 'Suburban NYC':
+        return 'Suburban NYC Resident'
+      case 'Outside the United States':
+        return 'International Tourist'
+      case 'USA (Outside NYC)':
+        return 'Domestic Tourist'
+      default:
+        return residence
+    }
   }
 }
